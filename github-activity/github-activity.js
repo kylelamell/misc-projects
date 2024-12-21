bb2ac3c130db13557da35c9c7c0c717b226107f8
kylelamell <kyle.lamell@uvm.edu>
2024-12-21 06:45:10 -0500

    comments

--- github-activity/github-activity.js
+++ github-activity/github-activity.js
@@ -1,16 +1,20 @@
 import { Octokit } from "@octokit/core";
 import { config } from "./config.js";
 
+// get command line inputs
 const args = process.argv.slice(2);
 const username = args[0];
 if (username) {
+  // display the username
   console.log("github username given: " + username);
   console.log("\n");
 
+  // stup octokit to interface with the github api
   const octokit = new Octokit ({
     auth: config.githubAPI
   });
 
+  // await the response from the api
   const response = await octokit.request("GET /users/{username}/events", {
     username: username,
     headers : {
@@ -18,10 +22,11 @@ if (username) {
     }
   });
 
+  // get the relevant data from the api
   const data = response.data;
 
+  // aggregate the data to only include the event type and the repo
   const dataAgg = [];
-
   for (const d of data) {
     dataAgg.push({
       type: d.type,
@@ -29,6 +34,7 @@ if (username) {
     })
   }
 
+  // store unique data and their counts
   const uniqueCounts = dataAgg.reduce((acc, curr) => {
     const key = `${curr.type}-${curr.name}`;
     if (acc[key]) {
@@ -39,7 +45,9 @@ if (username) {
     return acc;
   }, {});
   
+  // turn into array we can manip
   const result = Object.values(uniqueCounts);
   
+  // return results
   console.log(result);
 }
