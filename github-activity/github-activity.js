8b218e79a1220735af0390a6d5987bd63f21e0fb
kylelamell <kyle.lamell@uvm.edu>
2024-12-21 06:42:01 -0500

    api now returns the data fro the specified username, and we sort it by the event type and repo

--- github-activity/github-activity.js
+++ github-activity/github-activity.js
@@ -5,6 +5,7 @@ const args = process.argv.slice(2);
 const username = args[0];
 if (username) {
   console.log("github username given: " + username);
+  console.log("\n");
 
   const octokit = new Octokit ({
     auth: config.githubAPI
@@ -17,6 +18,28 @@ if (username) {
     }
   });
 
-  const data = JSON.parse(response);
-  console.log(data);
+  const data = response.data;
+
+  const dataAgg = [];
+
+  for (const d of data) {
+    dataAgg.push({
+      type: d.type,
+      repo: d.repo.name
+    })
+  }
+
+  const uniqueCounts = dataAgg.reduce((acc, curr) => {
+    const key = `${curr.type}-${curr.name}`;
+    if (acc[key]) {
+      acc[key].count++;
+    } else {
+      acc[key] = { ...curr, count: 1 };
+    }
+    return acc;
+  }, {});
+  
+  const result = Object.values(uniqueCounts);
+  
+  console.log(result);
 }
