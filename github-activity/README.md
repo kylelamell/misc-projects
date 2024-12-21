41b647e2594306265c4c3e2e10429ade162f07b3
kylelamell <kyle.lamell@uvm.edu>
2024-12-21 06:08:09 -0500

    hopefully this works

--- github-activity/README.md
+++ github-activity/README.md
@@ -1,2 +1,8 @@
 # github user activity
 this program will display the recent activity of a specified user via the github api
+
+## documentation referenced
+- github api
+  - https://docs.github.com/en/rest/activity/events?apiVersion=2022-11-28
+- Octokit.js
+  - https://github.com/octokit/core.js#readme
--- github-activity/github-activity.js
+++ github-activity/github-activity.js
@@ -1,10 +1,22 @@
+import { Octokit } from "@octokit/core";
 import { config } from "./config.js";
 
 const args = process.argv.slice(2);
-
 const username = args[0];
-
 if (username) {
   console.log("github username given: " + username);
-  console.log("githubs api key given: " + config.githubAPI);
+
+  const octokit = new Octokit ({
+    auth: config.githubAPI
+  });
+
+  const response = await octokit.request("GET /users/{username}/events", {
+    username: username,
+    headers : {
+      "X-GitHub-Api-Version": "2022-11-28"
+    }
+  });
+
+  const data = JSON.parse(response);
+  console.log(data);
 }
