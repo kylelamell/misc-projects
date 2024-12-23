8ae1e75425291e3e3ecb02951b577882f5fb60b4
kylelamell <kyle.lamell@uvm.edu>
2024-12-23 08:30:49 -0500

    initialized project

---
+++ personal-blog/README.md
@@ -0,0 +1,2 @@
+# personal blog project
+- personal blog with a publiic page that lets anyone view, and an admin side to post, delete, and edit.
---
+++ personal-blog/api/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhWsu-uVa-TohmNozPep8VvnfYbYyHJbY",
+  authDomain: "personal-blog-project-1b4ac.firebaseapp.com",
+  projectId: "personal-blog-project-1b4ac",
+  storageBucket: "personal-blog-project-1b4ac.firebasestorage.app",
+  messagingSenderId: "11076773238",
+  appId: "1:11076773238:web:7a20b578f82388969d4657"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
